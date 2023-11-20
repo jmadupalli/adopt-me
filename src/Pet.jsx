3fbb5b040d0ac6d8b906193d7f57d6e7ae7c36f3
jmadupalli <jayanth.madupalli@gmail.com>
2023-11-19 18:45:03 -0600

    Core React

---
+++ src/Pet.jsx
@@ -0,0 +1,21 @@
+const Pet = ({ name, animal, breed, location, images, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpeg";
+  if (images.length) {
+    hero = images[0];
+  }
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name}></img>
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
+  );
+};
+
+export default Pet;
